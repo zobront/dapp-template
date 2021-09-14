119841a0b85249f8c7939e4cdf11e43283dd9b2a
Zach Obront <zachobront@ZachTop.attlocal.net>
2021-09-14 09:59:15 -0500

    waffle included for testing

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,13 @@
+require("@nomiclabs/hardhat-waffle");
+
 const INFURA_URL = '';
 const ROPSTEN_PRIVATE_KEY = '';
 
 module.exports = {
   solidity: "0.8.4",
-  networks: {
-    ropsten: {
-      url: INFURA_URL,
-      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
-    }
+  // networks: {
+  //   ropsten: {
+  //     url: INFURA_URL,
+  //     accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+  //   }
 };
