a0c33116dd5ac1f7eda865c96100a06e1ad1a00f
Zach Obront <zachobront@ZachTop.attlocal.net>
2021-09-13 21:54:59 -0500

    template upload

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+const INFURA_URL = '';
+const ROPSTEN_PRIVATE_KEY = '';
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      url: INFURA_URL,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    }
+};
---
+++ test/Contract.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert')
+const { ethers } = require("hardhat");
+
+let contractName;
+
+beforeEach(async () => {
+  // do any setup needed, like creating a new contract instance
+});
+
+describe("Contract Name", () => {
+  it("should do xyz", async () => {
+    assert(1 == 1);
+  });
+});
